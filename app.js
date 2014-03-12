e3229d123302c4f7edbbd529225e1d7370e25061
Matt Lea <mlea@schematical.com>
2014-03-12 11:37:07 -0500

    testing

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(express.methodOverride());
 app.use(express.cookieParser('your secret here'));
 app.use(express.session());
 app.use(app.router);
-app.use(require('stylus').middleware(path.join(__dirname, 'public')));
+//app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
